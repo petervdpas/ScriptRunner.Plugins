4e4b116cf661f9bb3ed0e34dbc08850da0f30c9e
Peter van de Pas <peter.vdpas@protonmail.com>
2024-12-19 00:44:10 +0100

    added AssemblyExtensions

---
+++ ScriptRunner.Plugins/Extensions/AssemblyExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System.Linq;
+using System.Reflection;
+
+namespace ScriptRunner.Plugins.Extensions;
+
+/// <summary>
+/// Provides extension methods for working with assemblies and their metadata.
+/// </summary>
+public static class AssemblyExtensions
+{
+    /// <summary>
+    /// Retrieves the public key token of an <see cref="AssemblyName"/> as a hexadecimal string.
+    /// </summary>
+    /// <param name="assemblyName">The <see cref="AssemblyName"/> to retrieve the token from.</param>
+    /// <returns>The public key token as a hexadecimal string, or <c>null</c> if no token is present.</returns>
+    public static string? GetPublicKeyTokenString(this AssemblyName assemblyName)
+    {
+        var tokenBytes = assemblyName.GetPublicKeyToken();
+        return tokenBytes != null
+            ? string.Join(string.Empty, tokenBytes.Select(b => b.ToString("x2")))
+            : null;
+    }
+}
